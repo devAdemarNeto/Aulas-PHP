5b221e5542429fb25a941028e580ad970cff322b
Ademar Neto <ademarnetodev@gmail.com>
2024-04-18 21:01:08 -0300

    Segundo commit aulas php

--- if.php
+++ if.php
@@ -1,4 +1,12 @@
 <?php
-     
 
+    $nome = 'Vitor';
+    $email='';
+
+    if($nome != '' || $email != ''){
+        echo "Cadastro efetuado com sucesso!";
+    }else{
+        echo "Por favor, preencha os dados obrigatórios";
+    }
 ?>
+
